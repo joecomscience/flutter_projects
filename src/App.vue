af323a869784b4124423391dfb9a35b6fb787099
Joewalker <assassin.joe@hotmail.com>
2018-08-11 14:54:45 +0700

    revert code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Navbar />
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
+    <Footer />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Footer from '@/components/Footer.vue';
+import Navbar from '@/components/Navbar.vue';
+
+@Component({
+  components: {
+    Footer,
+    Navbar,
+  },
+})
+export default class App extends Vue {
+
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+</script>
+
+<style lang="scss">
+  @import '../node_modules/bulma/css/bulma.css';
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
+
+  Footer {
+    margin-top: 3rem;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="columns">
+      <div v-for="item in contents">
+        <div class="column">
+          <Card v-bind:name="item.name" v-bind:content="item.content" v-bind:createdDate="item.createdDate" />
+        </div>
+      </div>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Card from '@/components/Card.vue';
+
+
 
 @Component({
   components: {
     HelloWorld,
+    Card,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+  public data() {
+    return {
+      contents: [
+        {
+          name: 'John Smith',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+          createdDate: '11:09 PM - 1 Jan 2016',
+        },
+        {
+          name: 'John Smith',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+          createdDate: '11:09 PM - 1 Jan 2016',
+        },
+        {
+          name: 'John Smith',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+          createdDate: '11:09 PM - 1 Jan 2016',
+        },
+      ],
+    };
+  }
+}
 </script>
