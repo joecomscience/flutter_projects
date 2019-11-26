d199941f0d0b0094daedb6d05a922c6c867567d4
joewalker <assassin.joe@hotmail.com>
2019-11-26 17:35:19 +0700

    create application layout demo

---
+++ layout_demo/lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme
+        .of(context)
+        .primaryColor;
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.stars,
+            color: Colors.red,
+          ),
+          Text('41')
+        ],
+      ),
+    );
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildBottomColumn(color, Icons.call, 'CALL'),
+          _buildBottomColumn(color, Icons.near_me, 'ROUTE'),
+          _buildBottomColumn(color, Icons.share, 'SHARE')
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    Widget imageSection = Image.asset(
+      'images/lake.jpg',
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Layout Demo'),
+          ),
+          body: Column(
+            children: [
+              imageSection,
+              titleSection,
+              buttonSection,
+              textSection,
+            ],
+          )
+      ),
+    );
+  }
+
+  Column _buildBottomColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color,),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
